32cc3cc50a47d049c2f2e1d09f99ffa5829b4322
Eric Nie <niee16@student.westboroughk12.org>
2018-12-25 21:00:12 -0500

    Change all likes to sad

--- content.js
+++ content.js
@@ -1,11 +1,8 @@
 console.log("Start"); 
 document.addEventListener('DOMContentLoaded', () => {
     setInterval(() => {
-        console.log("trying");
         var remove_reacts = document.querySelectorAll('*[aria-label="Love"], *[aria-label="Haha"], *[aria-label="Wow"], *[aria-label="Angry"], *[aria-label="Like"]'); 
         // var remove_reacts = document.querySelectorAll('.uiContextualLayerParent'); 
-        console.log(remove_reacts); 
-        console.log("Number of items to remove: " + remove_reacts.length); 
         Array.prototype.forEach.call(remove_reacts, function( node ) {
             node.parentNode.removeChild(node);
         });
@@ -13,12 +10,50 @@ document.addEventListener('DOMContentLoaded', () => {
         // var remove_icons = document.getElementsByClassName('_3j7l _2p78 _9-- _hly');
         let reacts = ['_3j7l', '_3j7m', '_3j7n', '_3j7o', '_3j7q'];
         var remove_icons = document.querySelectorAll('._3j7l, ._3j7m, ._3j7n, ._3j7o, ._3j7q');
-        console.log(remove_icons);
         Array.prototype.forEach.call(remove_icons, function( node ) {
             for (let i = 0; i < reacts.length; i++) {
                 node.classList.remove(reacts[i]);
             }
             node.classList.add('_3j7r');
         });
+
+        var like_buttons = document.querySelectorAll('i.sp_Gxq2MXbT8dg_2x'); 
+        console.log("Number of like buttons: " + like_buttons.length); 
+        Array.prototype.forEach.call(like_buttons, function( node ) {
+            node.classList.remove("sx_43c42c"); 
+            node.classList.remove("sp_Gxq2MXbT8dg_2x"); 
+            node.classList.add("sad-icon"); 
+        });
+
+        var like_texts = document.querySelectorAll('a[data-testid="UFI2ReactionLink"]'); 
+        console.log("Number of like texts: " + like_texts.length);
+        Array.prototype.forEach.call(like_texts, function( node ) {
+            node.lastChild.nodeValue = "Sad"; 
+        });
     }, 10); 
 });
+
+
+
+
+
+
+
+/*
+.sp_Gxq2MXbT8dg_2x.sx_43c42c
+    width: 18px;
+    height: 18px;
+    background-position: -38px -1187px;
+
+
+    .sp_Gxq2MXbT8dg_2x
+    background-image: url(/rsrc.php/v3/ym/r/Z2RVuHqAilK.png);
+    background-size: 97px 2042px;
+    background-repeat: no-repeat;
+    display: inline-block;
+    height: 16px;
+    width: 16px;
+
+    data-testid="UFI2ReactionLink"  -> innerhtml Sad
+
+    */
