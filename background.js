0ff15abb8178d1203f15c63be6daf5b7f6583369
Eric Nie <niee16@student.westboroughk12.org>
2018-12-28 17:02:10 -0500

    Fixed race condition in toggle

--- background.js
+++ background.js
@@ -3,22 +3,21 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.local.set({'on': 'true'});
 });
 
-
 chrome.browserAction.onClicked.addListener((tab) => {
     console.log("icon clicked"); 
      chrome.storage.local.get('on', (data) => {
-        console.log("Current status: " + data.on); 
-        if (data.on === 'false') {
-            //send message
-            chrome.browserAction.setIcon({path: "/sad-icons/sad-128.png"});
-            chrome.storage.local.set({'on': 'true'});
-        } else {
-            chrome.browserAction.setIcon({path: "/sad-icons/sad-128-grey.png"});
-            chrome.storage.local.set({'on': 'false'});
-        }
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {'task':'toggle'}, function(response) {
+                 if (data.on === 'false') {
+                    //send message
+                    chrome.browserAction.setIcon({path: "/sad-icons/sad-128.png"});
+                    chrome.storage.local.set({'on': 'true'});
+                } else {
+                    chrome.browserAction.setIcon({path: "/sad-icons/sad-128-grey.png"});
+                    chrome.storage.local.set({'on': 'false'});
+                }
             });
         });
+       
     });
 }); 
